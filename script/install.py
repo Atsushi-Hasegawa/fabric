a18208cf5595dc7e14f46ebf713d878f9a6e50f4
Atsushi-Hasegawa <vagrant@vagrant-centos65.vagrantup.com>
2015-04-11 21:07:55 +0000

    条件分岐を追加し、rollbackできるようにした

--- script/install.py
+++ script/install.py
@@ -1,34 +1,49 @@
-from fabric.api import run, task
+from fabric.api import run, task, sudo
 
 def install_mysql():
- run('sudo yum -y install mysql')
+  response = sudo('yum -y install mysql-server')
+  if response.failed:
+    uninstall_mysql()
+    abort("mysql install failed!")
 
 def install_jenkins():
-  run('sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo')
-  run('sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key')
-  run('sudo yum -y install jenkins')
+  response1 = sudo('wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo')
+  response2 = sudo('rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key')
+  response3 = sudo('yum -y install jenkins')
+  if response1.failed and response2.failed and response3.failed:
+      uninstall_jenkins()
+      abort("jenkins install failed")
 
 def install_git():
- run('sudo yum -y install git')
+  response = sudo('yum -y install git')
+  if response.failed:
+      uninstall_git()
+      abort("git install failed")
 
 def install_redis():
- run('sudo yum -y install redis')
+  response = sudo('yum -y install redis')
+  if response.failed:
+      uninstall_redis()
+      abort("redis install failed")
 
 def install_npm_nodejs():
- run('sudo rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm')
- run('sudo yum -y install npm nodejs --enablerepo=epel')
+  response1 = sudo('rpm -ivh --force http://ftp.riken.jp/Linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm')
+  response2 = sudo('yum -y install npm nodejs --enablerepo=epel')
+  if response1.failed and response2.failed:
+      uninstall_npm()
+      abort("npm nodejs install failed")
 
 def uninstall_mysql():
- run('sudo yum -y remove mysql')
+    sudo('yum -y remove mysql-server')
 
 def uninstall_jenkins():
-  run('sudo yum -y remove jenkins')
+    sudo('yum -y remove jenkins')
 
 def uninstall_git():
- run('sudo yum -y remove git')
+    sudo('yum -y remove git')
 
 def uninstall_redis():
- run('sudo yum -y remove redis')
+    sudo('yum -y remove redis')
 
 def uninstall_npm_nodejs():
- run('sudo yum -y remove npm nodejs')
+    sudo('yum -y remove npm nodejs')
